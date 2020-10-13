fa1930e146ca71dc252a0ba96c4400c3a7e2e1f1
Matias Arratibel <matias.arratibel@gmail.com>
2020-10-13 16:47:26 -0300

    Bug in importing pages fixed
    pagina portafolio y pagina servicios dieron error por escribir mal las mayusculas en la ruta, se puede ver en la edicion del archivo en la linea 6 y 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { ContactoComponent } from './contacto/contacto.component';
 import { HomeComponent } from './home/home.component';
 import { PartnersComponent } from './partners/partners.component';
-import { PortafolioComponent } from './Portafolio/Portafolio.component';
-import { ServicioComponent } from './Servicio/Servicio.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { ServicioComponent } from './servicio/servicio.component';
 
 const routes: Routes = [
 {path: 'home', component: HomeComponent},
 {path: 'contacto', component: ContactoComponent},
 {path: 'partners', component: PartnersComponent},
 {path: 'portafolio', component: PortafolioComponent},
-{path: 'servicio', component: ServicioComponent}];
+{path: 'servicio', component: ServicioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
