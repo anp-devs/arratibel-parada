9ae9c2513533f8b4110b825c5de93e69f85204f0
Matias Arratibel <matias.arratibel@gmail.com>
2020-10-13 15:54:13 -0300

    PAGINAS restantes
    contacto
    partnets
    portafolio
    servicio
    y añadidas a app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactoComponent } from './contacto/contacto.component';
+import { HomeComponent } from './home/home.component';
+import { PartnersComponent } from './partners/partners.component';
+import { PortafolioComponent } from './Portafolio/Portafolio.component';
+import { ServicioComponent } from './Servicio/Servicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'home', component: HomeComponent},
+{path: 'contacto', component: ContactoComponent},
+{path: 'partners', component: PartnersComponent},
+{path: 'portafolio', component: PortafolioComponent},
+{path: 'servicio', component: ServicioComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PartnersComponent } from './partners/partners.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { ServicioComponent } from './servicio/servicio.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PartnersComponent,
+    ContactoComponent,
+    ServicioComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule,
