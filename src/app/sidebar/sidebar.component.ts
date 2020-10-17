08bea0f83cc1e89b7fda2650c862fb73060c281a
Matias Arratibel <matias.arratibel@gmail.com>
2020-10-16 23:11:45 -0300

    Changes to sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -17,6 +17,24 @@ export class SidebarComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) {
+    //this.loadScripts(); 
+  }
+ 
+  // Method to dynamically load JavaScript 
+/*  loadScripts() { 
+ 
+    // This array contains all the files/CDNs 
+    const dynamicScripts = [ 
+       'assets/sticky-bar.js'
+    ]; 
+    for (let i = 0; i < dynamicScripts.length; i++) { 
+      const node = document.createElement('script'); 
+      node.src = dynamicScripts[i]; 
+      node.type = 'text/javascript'; 
+      node.async = false; 
+      document.getElementsByTagName('head')[0].appendChild(node); 
+    } 
+ } */
 
 }
---
+++ src/assets/sticky-bar.js
@@ -0,0 +1,36 @@
+/*body.onload(function() {
+    console.log("Hello works");
+    
+    var distancia = $('#bar').offset().top;
+
+    $(window).on('scroll', function() {
+
+        if ($(window).scrollTop() > distancia) {
+
+            $('#bar').addClass('sticky-bar');
+
+        } else if ($(window).scrollTop() <= 0 || $(window).scrollTop() < distancia) {
+
+            $('#bar').removeClass('sticky-bar');
+
+        }
+
+    })
+
+});*/
+
+/*window.onload = function() {
+
+    var distancia = document.getElementById('bar').offsetTop;
+    var barra = document.getElementById('bar');
+    window.onscroll = function() {
+        if (window.scrollY > 105) {
+
+            barra.classList.add('sticky-bar');
+
+        } else if (window.scrollY <= 0 || window.scrollY() < 105) {
+            barra.classList.remove('sticky-bar');
+        }
+    }
+
+}*/
